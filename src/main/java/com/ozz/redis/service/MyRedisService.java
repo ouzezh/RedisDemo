430a534c49703a1b6538660efa234fe3a1c37b47
ouzezh <ouzezh@163.com>
2022-02-28 15:53:23 +0800

    redis transaction

--- src/main/java/com/ozz/redis/service/MyRedisService.java
+++ src/main/java/com/ozz/redis/service/MyRedisService.java
@@ -33,19 +33,19 @@ public class MyRedisService {
     public boolean optimisticSet(String key, String version, Duration duration, String beforeVersion) {
         SessionCallback<List<Object>> callback = new SessionCallback<>() {
             @Override
-            public <K, V> List<Object> execute(@Nullable RedisOperations<K, V> operations) throws DataAccessException {
-                StringRedisTemplate rt = (StringRedisTemplate) operations;
-                rt.watch(key);
-                String ver = rt.opsForValue().get(key);
-                rt.multi();// 开启事务
+            public <K, V> List<Object> execute(@Nullable  RedisOperations<K, V> operations) throws DataAccessException {
+                StringRedisTemplate srt = (StringRedisTemplate) Objects.requireNonNull(operations);
+                srt.watch(key);
+                String ver = srt.opsForValue().get(key);
+                srt.multi();// 开启事务
                 try {
                     if (!MyRedisService.this.equals(beforeVersion, ver)) {
                         return Collections.emptyList();
                     }
-                    rt.opsForValue().set(key, version, duration);
-                    return rt.exec();// 执行任务队列里所有命令，并结束事务
+                    srt.opsForValue().set(key, version, duration);
+                    return srt.exec();// 执行任务队列里所有命令，并结束事务
                 } catch (Exception e) {
-                    rt.discard();// 取消事务并执行unwatch
+                    srt.discard();// 取消事务并执行unwatch
                     throw e;
                 }
             }
